38144f3858468061ab6b031e3a5d91cd67a95199
HugoDtGomes <150141664+HugoDtGomes@users.noreply.github.com>
2026-07-07 16:02:23 -0300

    more buttons

--- scr/main.py
+++ scr/main.py
@@ -15,7 +15,7 @@ def open_file(window, text_edit):
     
     
 
-def saveAs_file(window, text_edit):
+def save_file_as(window, text_edit):
     filepath =asksaveasfilename(filetypes=[("Text Files", "*.txt")])
     
     if not filepath:
@@ -25,31 +25,58 @@ def saveAs_file(window, text_edit):
         content = text_edit.get(1.0, tk.END)
         f.write(content)
     window.title(f"Open File: {filepath}")
-    
+
+def save(window, text_edit):
+    pass
+
+def find_text(window, text_edit):
+    pass
+
+def replace_text(window, text):
+    pass
     
 
 def main():
     window = tk.Tk()
     window.title("Text Editor")
+    
+    # Configure grid weights
     window.rowconfigure(0, minsize=400)
     window.columnconfigure(1, minsize=500)
     
-    
+    # Create text
     text_edit = tk.Text(window, font="Helvetica 10 ")
     text_edit.grid(row=0, column=1)
     
+    # 
     frame = tk.Frame(window, relief=tk.RAISED, bd=2)
-    save_button = tk.Button(
-        frame, text="Save As", command=lambda: saveAs_file(window, text_edit))
+    
+    # Create Buttons
     open_button = tk.Button(
         frame, text="Open", command=lambda: open_file(window, text_edit))
+    save_button = tk.Button(
+        frame, text="Save", command=lambda: print(" test : save(window, text_edit)"))
+    save_file_as_button = tk.Button(
+        frame, text="Save As", command=lambda: save_file_as(window, text_edit))
+    exit_button = tk.Button(
+        frame, text="Exit", command=window.quit)
     
+    # Grid Buttons
+    open_button.grid(row=0, column=0, padx=5, pady=5, sticky= "ew")
+    save_button.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
+    save_file_as_button.grid(row=2, column=0, padx=5, pady=5, sticky="ew")
+    exit_button.grid(row=3, column=0, padx=5, pady=5, sticky="ew")
     
-    save_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
-    open_button.grid(row=1, column=0, padx=5, pady=5, sticky= "ew")
+    # Configure frame grid
     frame.grid(row=0, column=0, sticky="ns")
+    
+    # configure shortcuts
+    window.bind("<Control-s>", lambda x: save_file_as(window, text_edit))
+    window.bind("<Control-o>", lambda x: open_file(window, text_edit))
+    window.bind("<Control-w>", lambda x: window.quit())
     
     window.mainloop()
 
 
-main()
+if __name__ == "__main__":
+    main()
