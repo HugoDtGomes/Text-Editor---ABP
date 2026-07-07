631441837e5fa0abab125a51754c68d702ff92da
HugoDtGomes <150141664+HugoDtGomes@users.noreply.github.com>
2026-07-07 16:37:03 -0300

    changes on functions

--- scr/main.py
+++ scr/main.py
@@ -2,30 +2,42 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 def open_file(window, text_edit):
-    filepath = askopenfilename(filetypes=[("Text Files", "*.txt")])
+    filepath = askopenfilename(filetypes=[("Text Files", "*.txt"),("All Files", "*.*")])
     
     if not filepath:
         return
     
     text_edit.delete(1.0, tk.END)
-    with open(filepath, "r") as f:
-        content = f.read()
-        text_edit.insert(tk.END, content)
-    window.title(f"Open File: {filepath}")
-    
+    try:
+        with open(filepath, "r") as f:
+            content = f.read()
+            text_edit.insert(tk.END, content)
+        window.title(f"Open File: {filepath}")
+    except Exception as e:
+        text_edit.insert(tk.END, f"Error opening file: {e}")
     
 
 def save_file_as(window, text_edit):
-    filepath =asksaveasfilename(filetypes=[("Text Files", "*.txt")])
+    filepath = asksaveasfilename(filetypes=[("Text Files", "*.txt"),("All Files", "*.*")])
     
     if not filepath:
         return
     
-    with open(filepath , "w") as f:
-        content = text_edit.get(1.0, tk.END)
-        f.write(content)
-    window.title(f"Open File: {filepath}")
+    try:
+        with open(filepath , "w") as f:
+            content = text_edit.get(1.0, tk.END)
+            f.write(content)
+        window.title(f"Open File: {filepath}")
+    except Exception as e:
+        text_edit.insert(tk.END, f"Error saving file: {e}")
 
+
+def new_file(window, text_edit):
+    text_edit.delete(1.0, tk.END)
+    window.title("Text Editor ABP - Untiled")
+        
+    
+    
 def save(window, text_edit):
     pass
 
@@ -38,7 +50,7 @@ def replace_text(window, text):
 
 def main():
     window = tk.Tk()
-    window.title("Text Editor")
+    window.title("Text Editor ABP - Untiled")
     
     # Configure grid weights
     window.rowconfigure(0, minsize=400)
@@ -52,20 +64,23 @@ def main():
     frame = tk.Frame(window, relief=tk.RAISED, bd=2)
     
     # Create Buttons
+    new_file_button = tk.Button(
+        frame, text="New File", command= lambda:new_file(window, text_edit))
     open_button = tk.Button(
-        frame, text="Open", command=lambda: open_file(window, text_edit))
+        frame, text="Open", command= lambda: open_file(window, text_edit))
     save_button = tk.Button(
-        frame, text="Save", command=lambda: print(" test : save(window, text_edit)"))
+        frame, text="Save", command= lambda: print(" test : save(window, text_edit)"))
     save_file_as_button = tk.Button(
-        frame, text="Save As", command=lambda: save_file_as(window, text_edit))
+        frame, text="Save As", command= lambda: save_file_as(window, text_edit))
     exit_button = tk.Button(
-        frame, text="Exit", command=window.quit)
+        frame, text="Exit", command= window.quit)
     
     # Grid Buttons
-    open_button.grid(row=0, column=0, padx=5, pady=5, sticky= "ew")
-    save_button.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
-    save_file_as_button.grid(row=2, column=0, padx=5, pady=5, sticky="ew")
-    exit_button.grid(row=3, column=0, padx=5, pady=5, sticky="ew")
+    new_file_button.grid(row=0, column=0, padx=5, pady=5, sticky= "ew")
+    open_button.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
+    save_button.grid(row=2, column=0, padx=5, pady=5, sticky="ew")
+    save_file_as_button.grid(row=3, column=0, padx=5, pady=5, sticky="ew")
+    exit_button.grid(row=4, column=0, padx=5, pady=5, sticky="ew")
     
     # Configure frame grid
     frame.grid(row=0, column=0, sticky="ns")
