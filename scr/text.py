086e8e2957ac09b1020c451d27977ef68cbc3cd4
HugoDtGomes <150141664+HugoDtGomes@users.noreply.github.com>
2026-07-12 15:55:37 -0300

    Find text

--- scr/text.py
+++ scr/text.py
@@ -4,7 +4,7 @@ from tkinter import messagebox, simpledialog, Toplevel
 def find_text(window, text_edit):
     find_window = Toplevel(window)
     find_window.title("Buscar texto")
-    find_window.geometry("400x60")
+    find_window.geometry("400x120")
     find_window.resizable(False, False)
     find_window.transient(window)  # Ficar acima da janela principal
     find_window.grab_set() 
@@ -23,9 +23,44 @@ def find_text(window, text_edit):
             messagebox.showwarning("Digite o texto a ser buscado")
             return
         
-        text_edit.tag_remove("found",)
-        pass
-    pass
+        # Limpar selecao
+        text_edit.tag_remove("found", "1.0", tk.END)
+        
+        # 
+        start = "1.0"
+        found_count = 0
+        while True:
+            start = text_edit.search(search_text, start, stopindex= tk.END, nocase=True, exact=False)
+            if not start:
+                break
+            end = f"{start} + {len(search_text)}c"
+            text_edit.tag_add("found", start, end)
+            found_count +=1
+            start = end
+        
+        # Configurar estilo de destaque
+        text_edit.tag_config("found", background="yellow" , foreground="black") 
+        
+        if found_count > 0 :
+            text_edit.mark_set("insert", "1.0")
+            text_edit.see("1.0")
+            messagebox.showinfo("Busca", f"Encontrado {found_count} no texto") 
+        else:
+            messagebox.showinfo("Busca", "Texto nao encontrado")
+    
+    # fechar janela de busca
+    def close_find():
+        """Fecha a janela de busca e remove os destaques"""
+        text_edit.tag_remove("found", "1.0", tk.END)
+        find_window.destroy()
+    
+    # Botoes
+    button_frame = tk.Frame(find_window)
+    button_frame.grid(row=1, column= 0, columnspan=2, pady=10)
+    
+    tk.Button(button_frame, text="Buscar", command= do_find, width=10).pack(side=tk.LEFT, padx=5)
+    tk.Button(button_frame, text="Fechar", command= close_find, width=10).pack(side=tk.LEFT, padx=5) 
+   
 
 def replace_text(window, text):
     pass
